6a4359f78d40e7de6d0a8432fc3c87e0f1f96dfc
ddowns15 <37954010+ddowns15@users.noreply.github.com>
2018-05-17 18:49:36 -0700

    Lecture 46 done
    added local storage

--- src/app.js
+++ src/app.js
@@ -6,8 +6,29 @@ class IndecisionApp extends React.Component {
     this.handleAddOption = this.handleAddOption.bind(this);
     this.handleDeleteOption = this.handleDeleteOption.bind(this);
     this.state = {
-      options: props.options
+      options: []
     };
+  }
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+      if (options) {
+        this.setState(() => ({ options}));
+      }
+    } catch (e) {
+      //Do nothing at all
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+      console.log("saving data");
+    }
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount!');
   }
   handleDeleteOptions() {
     this.setState(() => ({ options: [] }));
@@ -53,9 +74,10 @@ class IndecisionApp extends React.Component {
   }
 }
 
-IndecisionApp.defaultProps = {
-  options: []
-};
+// No longer needed since implementing stored values 
+// IndecisionApp.defaultProps = {
+//   options: []
+// };
 
 const Header = (props) => {
   return (
@@ -87,6 +109,7 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && <p>Please add an option to get started!</p>}
       {
         props.options.map((option) => (
           <Option key={option} optionText={option} handleDeleteOption={props.handleDeleteOption}/>
@@ -126,6 +149,10 @@ class AddOption extends React.Component {
     const error = this.props.handleAddOption(option);
 
     this.setState(() => ({error}));
+
+    if (!error) {
+      e.target.elements.option.value = '';
+    }
   }
   render() {
     return (
